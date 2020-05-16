793e2e677a61c9c81ddfb11b1df32a589731bc9a
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-16 13:36:16 +0530

    added updates function to updates css variables
    added updates function to updates css variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdates() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdates));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdates));
